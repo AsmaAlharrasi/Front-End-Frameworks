e59cf787c6e24dceb17d8a430a10c51d404f6750
AsmaAlharrasi <asmaalharrasi18@gmail.com>
2024-02-01 19:13:49 +0400

    add todoapp

---
+++ React/todoapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ToDoList from './component/ToDoList';
+
+function App() {
+  return (
+    <>    
+    <div className="todo-app">
+     <ToDoList />
+    </div>
+    </>
+  );
+}
+
+export default App;
